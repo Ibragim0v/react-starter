aaa233f032e3ab577a12e7ed9e7bb625489bd066
Ibragim0v <ibragimov.amirshoh@mail.ru>
2022-05-19 19:45:26 +0500

    feat: react-starter-finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,3 @@
-import Button from "@mui/material/Button"
-
 export function App() {
-    return (
-        <div className="App">
-            <Button onClick={() => console.log("hello")} variant="contained">
-                next
-            </Button>
-        </div>
-    )
+    return App
 }
