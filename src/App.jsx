e21b95fd1f2581c2a31ae426d2caeeca31ebf7fb
Ibragim0v <ibragimov.amirshoh@mail.ru>
2022-05-30 17:30:47 +0500

    feat(fix): add craco and update-something

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,3 @@
 export function App() {
-    return <>App</>
+    return <div className="App">App</div>
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,14 @@
 import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
+import { GlobalStyles } from "./styles/global-style"
 import { App } from "./App"
 import "bootstrap/dist/js/bootstrap"
-import "bootstrap/dist/css/bootstrap.min.css"
 
 const root = createRoot(document.getElementById("root"))
 
 root.render(
     <BrowserRouter>
+        <GlobalStyles />
         <App />
     </BrowserRouter>
 )
